8b5bdcbbbe6297d7367c5e4c976d2c3982631ae6
Marko Perović <mmarko.perovici3@gmail.com>
2021-02-24 21:25:16 +0100

    Add files via upload

--- code-behind.js
+++ code-behind.js
@@ -17,8 +17,27 @@ var contactHeader = document.querySelector('.contact-header');
 
     //open new page for options from header
     function openPage(id) {
-       window.location.href = "form.html";
-       sessionStorage.setItem("id",id);
+      sessionStorage.setItem("pageId",id);
+      var get = sessionStorage.getItem("pageId");
+      if(get == 3) {
+         window.location.href= "contact-us.html";
+      }else if (get == 4) {
+         window.location.href= "index.html";
+      }else if (get == 2) {
+         window.location.href= "create-account.html";
+      }
     }
 
-
+    //send email
+    document.querySelector('#submit').addEventListener('click',() => {
+      Email.send({
+               Host:"smtp.mailtrap.io",
+               Username:"3614ca97b06892",
+               Password:"f66ed6c438d0a3",          
+               To:"mmarko.perovici3@gmail.com",
+               From:document.getElementById('mail-input').value,
+               Subject:document.getElementById("subject-input").value,
+               Body:"From:  " + document.getElementById('mail-input').value + "<br><br>" + "Message:  " + document.getElementById("message-input").value + "<br><br>",
+        }).then(mesage => alert(mesage)
+        );
+      })
