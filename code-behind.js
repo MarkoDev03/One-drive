30a1683b0ba5eeddb50cca684795c1d35cb7ffbf
Marko Perović <mmarko.perovici3@gmail.com>
2021-02-24 01:56:29 +0100

    Add files via upload

--- code-behind.js
+++ code-behind.js
@@ -1,16 +1,24 @@
 var contactHeader = document.querySelector('.contact-header');
 
+     //get data from json file for options in header
      fetch('data.json',{method:"GET"})
      .then(response => response.json())
      .then(data => window.onload = fncHeader(data))
-     .catch(error => console.error());
+     .catch(error => console.error(error));
 
 
+     //show header options on index page
      function fncHeader(data) {
         var item = data.header;
        for(var i = 0; i < item.length; i++) {
-        contactHeader.innerHTML += `<a href="" class="${item[i].class}">${item[i].name}</a>`;
+        contactHeader.innerHTML += `<a class="${item[i].class}" onclick="openPage(${item[i].id})">${item[i].name}</a>`;
        }       
+    }
+
+    //open new page for options from header
+    function openPage(id) {
+       window.location.href = "form.html";
+       sessionStorage.setItem("id",id);
     }
 
 
