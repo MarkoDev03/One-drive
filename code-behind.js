7b87c0177e8f2bb41f688f520da5f33a208f1462
Marko Perović <mmarko.perovici3@gmail.com>
2021-02-25 17:00:04 +0100

    Add files via upload

--- code-behind.js
+++ code-behind.js
@@ -31,4 +31,38 @@ var contactHeader = document.querySelector('.contact-header');
     }
 
     //create account
-   
+    var firebaseConfig = {
+      apiKey: "AIzaSyBInVpEsEvXzZtYDLy7qNA4AOIaS-5mjb8",
+      authDomain: "saveprojects-e2b7f.firebaseapp.com",
+      projectId: "saveprojects-e2b7f",
+      storageBucket: "saveprojects-e2b7f.appspot.com",
+      messagingSenderId: "1068677321956",
+      appId: "1:1068677321956:web:97a4d0b9ddeb63520588f1",
+      measurementId: "G-HXW1NYY74D"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+    const auth = firebase.auth();
+  
+  //create account
+    function createAccount(){
+        let email = document.getElementById('e-mail-input');
+        let password = document.getElementById('password-input');
+        const promise = auth.createUserWithEmailAndPassword(email.value, password.value);
+        promise.catch(e => alert(e.message));
+        alert("si");
+      }
+  
+      //send email
+      function sendMail() {
+         Email.send({
+            Host:"smtp.mailtrap.io",
+            Username:"3614ca97b06892",
+            Password:"f66ed6c438d0a3",          
+            To:"mmarko.perovici3@gmail.com",
+            From:document.getElementById('mail-input').value,
+            Subject:document.getElementById("subject-input").value,
+            Body:"From:  " + document.getElementById('mail-input').value + "<br><br>" + "Message:  " + document.getElementById("message-input").value + "<br><br>",
+     }).then(mesage => alert(mesage)
+     );
+   }
