c7a85fa23bf9581f73f881cb834e946030b5f435
Marko Perović <mmarko.perovici3@gmail.com>
2021-03-29 15:51:35 +0200

    Add files via upload

--- code-behind.js
+++ code-behind.js
@@ -1213,29 +1213,51 @@ function closeALert() {
       document.getElementById('postcontent').innerHTML = '';
    }
 
+   var emojis = [0x1F600, 0x1F604, 0x1F34A, 0x1F344, 0x1F37F, 0x1F363, 0x1F370, 0x1F355,
+      0x1F354, 0x1F35F, 0x1F6C0, 0x1F48E, 0x1F5FA, 0x23F0, 0x1F579, 0x1F4DA,
+      0x1F431, 0x1F42A, 0x1F439, 0x1F424];
+var ardArray = [];
+for(var i=0;i<emojis.length;i++){
+      ardArray.push({
+         code:emojis[i],
+         content: String.fromCodePoint(emojis[i]),
+         id: i,
+       });}
+     console.log(ardArray);
 
+
+    
 
 
 firebase.auth().onAuthStateChanged(function(user) {var user = firebase.auth().currentUser;
 if(user) {
    firebase.storage().ref("users/" + user.uid +'/profile_image' + '/profile_image.jpg').getDownloadURL().then(x =>{
-      //console.log("url:"+imgurl);
+      
       document.getElementById('sendMessage').addEventListener('click',(e)=>{
         
          var username = user.email, didsplayname = username.slice(0, -10);
          var message = document.getElementById('text-message-user').value;
  if(message != ""){
+ 
+   
         firebase.database().ref("messages").push().set({
             "sender":didsplayname,
             "message":message,
             "profileimage":x,
-         })
+        })
       }
       document.getElementById('text-message-user').value = ''
     })})}})
 
 
 
-  
+
+
+     for(var i=0;ardArray.length;i++){
+   document.getElementById('emojis').innerHTML += `<p onclick="setIcon(${ardArray[i].code})">${ardArray[i].content}</p>`;
+}
+
+function setIcon(icon) {document.getElementById('text-message-user').value+=String.fromCodePoint(icon)}
+
 
 
